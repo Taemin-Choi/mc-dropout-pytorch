6d9048c19b98bd1c5a45ba84a29b2e6e6b3f0d13
Taemin <choitm0707@gmail.com>
2020-12-12 18:07:49 +0900

    Update
    README.md and regression/train.py

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mc-dropout-pytorch
-mc-dropout-pytorch
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/Taemin-Choi/mc-dropout-pytorch/LICENSE) [![Python 3.5+](https://img.shields.io/badge/python-3.5+-blue.svg)](https://www.python.org/downloads/release/python-2716/)
+[![Pytorch 1.0](https://img.shields.io/badge/pytorch-1.0.1-blue.svg)](https://pytorch.org/)
+
+Pytorch implementation of MC Dropout(also called Dropout Sampling) for the following examples:
+* Regression
+* ~~Classification~~
+* ~~Object Detection~~
+
+## Prerequisites
+* PyTorch
+* Numpy
+* Matplotlib
+
+This package is written in python 2.7 and Pytorch 1.0.1
+Also, it is wrriten under the premise that CUDA is available.
+
+## Usage
+### 1. Regression
---
+++ regression/train.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+use_gpu = torch.cuda.is_available()
+print("GPU {}".format(use_gpu))
+print("GPU count {}".format(torch.cuda.device_count()))
+device_index =torch.cuda.current_device()
+print("Using {}".format(torch.cuda.get_device_name(device_index)))
+
+# For reproducivility
+torch.manual_seed(777)
+
+# categories = ['line', 'curve', 'circle']
+
+# # line curve and circle
+
+# # X and Y
+# x1 = np.linspace(0, 6, 90)
+# x2 = np.linspace(8, 10, 30)
+
+# x_train = np.append(x1, x2)
+# y_train = np.sin(x_train) + 0.1
+
+# x_train_reshape = np.reshape(x_train, (-1, 1))
+# y_train_reshape = np.reshape(y_train, (-1, 1))
+# X = Variable(torch.Tensor(x_train_reshape))
+# Y = Variable(torch.Tensor(y_train_reshape))
+
+# # Model
+# model = nn.Sequential(nn.Linear(1, 50, bias=True),
+#                       nn.Sigmoid(),
+#                       nn.Linear(50, 50, bias=True),
+#                       nn.Sigmoid(),
+#                       nn.Linear(50, 1, bias=True))
+# print(model)
+
+# # cost criterion
+# criterion = nn.MSELoss()
+
+# # Minimize
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+# # Train the model
+# model.train()
+# for step in range(100001):
+#     optimizer.zero_grad()
+#     # Our hypothesis
+#     hypothesis = model(X)
+#     cost = criterion(hypothesis, Y)
+#     cost.backward()
+#     optimizer.step()
+
+#     if step % 20 == 0:
+#         print(step, cost.data.numpy())
+
+# # x_test = [2, 4, 6, 8]
+# # y_test = []
+
+# x_test = np.linspace(0, 8, 120)
+# x_test_reshape = np.reshape(x_test, (-1, 1))
+# X_test = Variable(torch.Tensor(x_test_reshape))
+
+# predicted = model(X_test)
+# y_test = predicted.data.numpy()
+# print(x_test.shape)
+# print(y_test.shape)
+# #     y_test.append(output_y)
+# # print(y_test)
+# # # # x_test = np.array(x_test)
+# # # # y_test = np.array(y_test)
+
+# # # print(x_test)
+# # # print(y_test)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(x_train, y_train, c='b')
+# ax.scatter(x_test, y_test, c='r')
+# plt.show()
+
+class Trainer(object):
+    """
+    """
+    def __init__(self):
+        pass
+    def train(self):
+        pass
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shape', default="line", type=str)
+    args = parser.parse_args()
+
+    trainer = Trainer()
+    print(args.shape)
+
+
+
+    # solver = Solver(yolo, pascal)
+
+    # print('Start training ...')
+    # solver.train()
+    # print('Done training.')
+
+    # python train.py --weights YOLO_small.ckpt --gpu 0
